fed1e366f532e037fe1060fe557d913bb4608582
Svetlana Filimonova <lana.shvedova@gmail.com>
2017-11-28 23:03:49 +0300

    resizing is ready but it's weird

--- text/main.js
+++ text/main.js
@@ -1,29 +1,31 @@
 var blueDiv = document.getElementById("blue");
 
 blueDiv.style.cssText= "background-color: #A0DFD1; \
-width: 400px; \
+box-sizing: border-box; \
+width: 396px; \
 height: 200px;\
 border: 1px solid #7AB6A8;\
-position: relative; \ " ;
+position: relative; \ ";
 
 var whiteDiv = document.querySelector(".white");
 
 whiteDiv.style.cssText = " width: 370px; \
-height: 170px; \
+box-sizing: border-box; \
 border: 1px solid #7AB6A8;\
 color: #7AB6A8;\
-padding: 5px;\
+padding: 10px;\
 position: absolute;\
 background-color: white;\
-top: 18px;\
-left: 17px; \ ";
+top: 5%;\
+left: 3%; \ ";
 
 var yellowDiv = document.querySelector(".header");
 yellowDiv.style.cssText = "color: black;\
 background: -moz-linear-gradient(top, #ffd600 0%, #fcc826 100%); \
 background: -webkit-linear-gradient(top, #ffd600 0%,#fcc826 100%); \
 padding: 10px;\
-width: 360px;\
+box-sizing: border-box; \
+width: 368px;\
 position: absolute;\
 top: 0;\
 left: 0;\
@@ -47,41 +49,38 @@ yellowTrngl.style.cssText = "position: absolute;\
  text.style.marginTop = "50px";
 
 
-var windowWidthOld = window.innerWidth;
-console.log("before " + windowWidthOld);
+var windowWidthOld =  window.innerWidth;
+var windowWidthRatio = 1;
+var blueDivWidth =  blueDiv.offsetWidth;
+var whiteDivWidth = whiteDiv.offsetWidth;
+var yellowDivWidth = yellowDiv.offsetWidth;
+var blueDivHeight = blueDiv.offsetHeight;
+var whiteDivHeight = whiteDiv.offsetHeight;
+
+console.log(whiteDivHeight + " white, " + blueDivHeight + " blue, ");
 
  window.onload = window.onresize = function () {
 
-    var blueDivWidth = blueDiv.offsetWidth;
-    
-    console.log(blueDivWidth);
+    var windowWidthNew =  window.innerWidth;
 
-    var windowWidthNew = window.innerWidth;
+    windowWidthRatio = 1/(windowWidthOld / windowWidthNew);
 
-    var windowWidthRatio = windowWidthOld / windowWidthNew;
+    var newBlueDiv = blueDivWidth * windowWidthRatio;
+    var newWhiteDiv = whiteDivWidth * windowWidthRatio;
+    var newYellowWidth = yellowDivWidth * windowWidthRatio;
+    var whiteDivHeightAfter = whiteDiv.offsetHeight;
+    var heightRatio = whiteDivHeightAfter / whiteDivHeight;
+    var newBlueDivHeight = blueDivHeight * heightRatio;
 
-    console.log("after "+ windowWidthNew);
-    console.log("ratio "+ windowWidthRatio);
-
-    var newBlueDiv = blueDivWidth - windowWidthRatio;
-
-    console.log("new "+ newBlueDiv);
+    console.log(whiteDivHeightAfter + " after, " + heightRatio + " ratio heeight: " + newBlueDivHeight );
 
         blueDiv.style.width = newBlueDiv + "px";
-    
+        whiteDiv.style.width = newWhiteDiv + "px";
+        yellowDiv.style.width = newYellowWidth + "px";
 
-  
+        if (whiteDivHeightAfter >= blueDivHeight) {
+            blueDiv.style.height = newBlueDivHeight + "px";
+        }
 
 
-
-    
-   
-
-    // if (left.offsetHeight < window_height) {
-    //     left.style.height = '100%';    
-    //     left_height=left.offsetHeight;
-    //     console.log(left_height);
-    //     left.style.height = left_height + "px";
-
-    // } else {}
 };
