df1eb500865e75f965f7226c10801ff13c9da60d
epam <epam@local.rsu.edu.ru>
2017-11-30 21:15:00 +0300

    started the task

--- js_task_5/js_task5_1.js
+++ js_task_5/js_task5_1.js
@@ -1,36 +1,36 @@
-function calcIt() {
-  var result = 0;
+// function calcIt() {
+//   var result = 0;
 
-  var calculator = {
-    add: function(a) {
-      result += a;
-      var func = function(b) {
-        return result = b+a;
-      };
-      return func;
-    },
-    subtract: function(b) {
-        result -= a;
-        var func = function(a) {
-          return (result -= a);
-        };
-        return func;
-    },
-    divide: function(b) {
-      return (result /= b);
-    },
-    mutiply: function(a) {
-      return (result *= a);
-    },
-    getResult: function() {
-      return result;
-    }
-  };
+//   var calculator = {
+//     add: function(a) {
+//       result += a;
+//       var func = function(b) {
+//         return result = b + a;
+//       };
+//       return func;
+//     },
+//     subtract: function(b) {
+//         result -= a;
+//         var func = function(a) {
+//           return (result -= a);
+//         };
+//         return func;
+//     },
+//     divide: function(b) {
+//       return (result /= b);
+//     },
+//     multiply: function(a) {
+//       return (result *= a);
+//     },
+//     getResult: function() {
+//       return result;
+//     }
+//   };
 
-  return calculator;
-}
+//   return calculator;
+// }
 
-var calculate = calcIt();
+// var calculate = calcIt();
 
 // calculate();
 
@@ -43,4 +43,34 @@ var calculate = calcIt();
 
 
 
-console.log(calculate.add(4)(3));
+// console.log(calculate.add(4)(3));
+
+
+// THIS
+
+function Calculator() {
+  
+  // var result = 0;
+  
+  this.add = function(a) {
+     var result = a;
+    var func = function(b){
+      return a + b;
+    };
+    return func;
+
+
+  },
+
+  this.subtract =  function(a){
+  
+    var func = function(b) {
+    return (result -= b);
+    };
+    return func;
+     }
+}
+
+var calc = new Calculator;
+
+console.log(calc.add(4)(5));
