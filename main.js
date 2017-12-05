0b8d345eb1ff37ca613fdc75fc5ec3c8f3ba6c4b
Svetlana Filimonova <lana.shvedova@gmail.com>
2017-12-05 16:07:54 +0300

    calc doesn't do anthng

--- main.js
+++ main.js
@@ -39,12 +39,13 @@ function Calculator() {
     this.check = function(){
       return result;
     }
-  }
+  };
   
   var calculator = new Calculator;
 
+
   function createCalc() {
-      var wrapper = document.getElementById('wrapper');
+      var wrapper = document.querySelector('.wrapper');
       console.log(wrapper);
       var calc = document.createElement('div');
       calc.className = "calculator";
@@ -52,8 +53,9 @@ function Calculator() {
       console.log("load");
       var display = document.createElement("input");
       display.className = "display";
-      display.id = "display";
       display.disabled = true;
+      display.type = "text";
+      display.value = "0";
       calc.appendChild(display);
       console.log(display);
       var numOfRows = 5;
@@ -61,7 +63,6 @@ function Calculator() {
       var btnsInRow = 4;
       var btnNameIndex = 0;
       for(var i = 0; i < numOfRows; i++){
-
         var calcRow = document.createElement('div');
         calcRow.className = "calc__row";
         calc.appendChild(calcRow);
@@ -75,19 +76,84 @@ function Calculator() {
         for(var j = 0; j < btnsInRow; j++) {
             var calcBtns = document.createElement('button');
             calcBtns.className = "btn";
+            calcBtns.type = "button";
             calcBtns.innerHTML = btnNames[btnNameIndex];
             calcRow.appendChild(calcBtns);
             btnNameIndex ++;
+            // console.log(calcBtns);
         }
         
       };
         
+      
+      
+  };
 
+  // console.log(calcRow);
+ 
+  
+  document.addEventListener("DOMContentLoaded", createCalc);
+  document.addEventListener("DOMContentLoaded", getBtns);
+  
+  var calcWrapper = document.querySelector(".wrapper"); 
+  console.log(calcWrapper);
+  var addCalcBtn = document.getElementById('add');
+  addCalcBtn.addEventListener("click", createCalc);
+  
+  // calcDisplay.value = 0;
+
+
+calcWrapper.onclick = function(event) {
+  var target = event.target;
+  var calcDisplay = document.querySelector("input");
+  
+  console.log(calcDisplay);
+
+  if(target.className ==="btn" && target.innerHTML.match(/[0-9]/)) {
+    console.log("num");
+
+    if(calcDisplay.value == 0)
+    {calcDisplay.value = target.innerHTML;}
+    else{calcDisplay.value += target.innerHTML;}
 
   }
 
-  document.addEventListener("DOMContentLoaded", createCalc);
 
-  var addCalcBtn = document.getElementById('add');
 
-  addCalcBtn.addEventListener("click", createCalc);
+
+};
+  
+
+  
+
+function getBtns(){
+
+  var rows = document.getElementsByClassName('calc__row');
+  var btns = document.getElementsByClassName('btn');
+
+  console.log(rows.length);
+  console.log("btn lenght: " + btns.length);
+
+
+  // for(var i = 0; i < btns.length; i++) {
+
+  //   // console.log(btns[i].innerHTML);
+    
+  //   if(btns[i].innerHTML.match(/[0-9]/)){
+      
+  //     btns[i].addEventListener("click", function(e){
+  //       console.log("num");
+  //     });
+  //   };
+
+
+
+  // };
+  // console.log(didgits);
+  
+};
+  
+
+
+  
+
